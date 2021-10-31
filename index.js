6a9d49bf367fa6c7656cdd66ff1f2ab7828d5dc4
akikop <aki.koppinen@metropolia.fi>
2021-10-31 21:01:43 +0200

    Part 3 exercises 3.9-3.21 done

--- index.js
+++ index.js
@@ -1,115 +1,119 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 /**
- * Aki K. 21.10.2021
+ * Aki K. 31.10.2021
  */
 
-//setting up a custom morgan token that shows the request body data when the operation is a POST operation
+//setting up a custom morgan token that shows the req body data when the operation is a POST operation
 morgan.token('response-body', function (req, res) {
   if (req.method === 'POST') {
     return JSON.stringify(req.body)
   }
   return null
 })
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
 //setting up morgan with custom console message after CRUD operations are used
 //I deleted '-' before response-time, because it signifies null and is very confusing to see in the middle of the message
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :response-body'))
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>Find the persons data at /api/persons</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
-  const personsLength = persons.length
-  const dateNow = Date()
-  res.send(`<div><p>Phonebook has info for ${personsLength} people</p><p>${dateNow}</p></div>`)
+  Person.find({}).then(result => {
+    //console.log(`Database contains ${result.length} entries`)
+    const personsLength = result.length
+    const dateNow = Date()
+    res.send(`<div><p>Phonebook has info for ${personsLength} people</p><p>${dateNow}</p></div>`)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then(person => {
+    console.log(person)
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  newPersons = persons.filter(person => person.id !== id)
-  if (newPersons.length === persons.length - 1) {
-    persons = newPersons
-    response.status(204).end()
-  } else {
-    response.status(404).json({
-      error: 'No person by given id found'
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
     })
-  }
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-  //console.log(body)
-  //console.log(request.headers)
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  } else if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-  const person = {
-    id: generateId(),
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+  //console.log('BODY:', body)
+  //console.log('HEADERS:', req.headers)
+  const person = new Person ({
     name: body.name,
     number: body.number
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  })
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  const newId = Math.floor(Math.random()*1000)
-  console.log('Generated id:', newId)
-  return newId
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+//middlewaret ----------->
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+//Kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+
+app.use(errorHandler)
+
+//<--------------middlewaret
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+//There needs to be 3 or 5 arguments.
+if (process.argv.length !== 3 && process.argv.length !== 5) {
+  console.log(`Incorrect amount of arguments: ${process.argv.length}. GET needs 3 arguments, POST needs 5 arguments.`)
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://kiki-ko_21:${password}@cluster0.jabzf.mongodb.net/phonebook-app?retryWrites=true`
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+//if 3 arguments then only password was given, and will show print the contents of the database
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+//if 5 arguments then name and number were also given in addition to password, and will save them to database
+} else {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+  
+  person.save().then(response => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
